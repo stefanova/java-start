a3e6d7294cf897442f0f856d6de2751b0b00159f
Karolina Kreft <kar.kreft@gmail.com>
2017-07-25 21:54:13 +0200

    StarFoods basic views, routeProvider, foodsController and index.html

---
+++ StarFoods/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('RESTApp', ['ngRoute']);
+var url = 'http://localhost:8088/';
+
+app.config(function($routeProvider){
+  var path = './views';
+  $routeProvider
+  .when('/', {
+    templateUrl: path + 'main.html'
+  })
+  .when('/foods',{
+    templateUrl: path + 'foods.html',
+    controller: 'foodsController'
+  });
+});
+
+app.controller('foodsController', function($scope, $http){
+  $scope.message = "Lista wszystich dostępnych produktów. Jeżeli nie ma tego, czego szukasz <<dodaj>> produkt do bazy.";
+  $http({
+    url: url + 'foods',
+    dataType: 'json',
+    params: {}
+  }).then(function(success){
+    $scope.name=success.data.name;
+},  function(error){
+  console.error(error);
+});
+});
